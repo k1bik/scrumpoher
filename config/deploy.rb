0caac14d4a5c2642cd330c9dfa283a15f92961fe
Kirill Biller <biller.k@yandex.ru>
2025-06-26 23:18:25 +0300

    Update linked files in Capistrano deployment and configure Action Cable origins for production

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ set :deploy_to, "/home/deploy/apps/scrumpoher"
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/master.key", ".env", "config/database.yml", "config/puma.rb"
+append :linked_files, "config/master.key", ".env", "config/database.yml"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "vendor", "storage"
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,6 +44,11 @@ Rails.application.configure do
   # config.action_cable.url = "wss://example.com/cable"
   # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
 
+  config.action_cable.allowed_request_origins = [
+    "https://scrumpoher.ru",
+    "http://scrumpoher.ru"
+  ]
+
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   # Can be used together with config.force_ssl for Strict-Transport-Security and secure cookies.
   # config.assume_ssl = true
